30645c326abadd4d1aa939c7af93c7b1069872a8
Hasina RABOANARY <anisahrab043@gmail.com>
2026-02-01 23:16:13 +0400

    [feat] Create className

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import ReactDom from "react-dom/client";
 import React from 'react';
 
-const Header = () => {
-  const myStyle = {
-    color: 'red',
-    backgroundColor: 'lightgrey',
-  };
-  return (
-    <>
-      <h1 style={myStyle}>Red red</h1>
-    </>
-  );
-};
+const Element = <h1 className="myclass">Hello World!</h1>;
 
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<Header />);
+root.render(Element);
