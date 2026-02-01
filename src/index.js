3b5d89cff6401a866185c41d3b50801d27229490
Hasina RABOANARY <anisahrab043@gmail.com>
2026-02-01 23:18:15 +0400

    [feat] Writing condition

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import ReactDom from "react-dom/client";
 import React from 'react';
 
-const Element = <h1 className="myclass">Hello World!</h1>;
+const x = 5;
+let text = 'Goodbye'
+if(x < 10){
+  text = 'If condition'
+}
+const Element = <h1>{text}</h1>;
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(Element);
