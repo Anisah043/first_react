f90647c8173a83401b128fb69a14022e1ed8dc65
Hasina RABOANARY <anisahrab043@gmail.com>
2026-02-03 23:12:58 +0400

    [feat] Learn components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import ReactDom from "react-dom/client";
 import React from 'react';
 
-const x = 5;
+// Class component
+// class Car extends React.Component {
+//   render() {
+//     return <h2>I am a Car!</h2>;
+//   }
+// }
+// Functional component
+function Car(props){
+  return <h2>I am a {props.color} Car!</h2>;
+}
 
-const Element = <h1>{(x) <10 ? "If statement!": "GoodBye"}</h1>;
-
-const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(Element);
+function Garage() {
+  return (
+    <>
+      <h1>Who lives in my garage?</h1>
+      <Car color="red"/>
+      <Car color="green"/>
+      <Car color="blue"/>
+    </>
+  );
+}
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(<Garage/>);
