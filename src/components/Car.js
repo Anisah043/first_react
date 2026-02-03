4cbceff71a15a8704a1acfe7cb012e1979ae1cf5
Hasina RABOANARY <anisahrab043@gmail.com>
2026-02-03 23:19:52 +0400

    [feat] create file for separate component to the index.js

---
+++ src/components/Car.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Car(){
+    return <h2>I am a Car!</h2>;
+}
+
+export default Car;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDom from "react-dom/client";
 import React from 'react';
-
+import Car from "./components/Car";
 // Class component
 // class Car extends React.Component {
 //   render() {
@@ -8,19 +8,6 @@ import React from 'react';
 //   }
 // }
 // Functional component
-function Car(props){
-  return <h2>I am a {props.color} Car!</h2>;
-}
 
-function Garage() {
-  return (
-    <>
-      <h1>Who lives in my garage?</h1>
-      <Car color="red"/>
-      <Car color="green"/>
-      <Car color="blue"/>
-    </>
-  );
-}
 const root = ReactDom.createRoot(document.getElementById('root'));
-root.render(<Garage/>);
+root.render(<Car/>);
