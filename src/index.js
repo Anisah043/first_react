4d1cb0fbb221aa3c4216407ab5dcfeefe0f899ab
Hasina RABOANARY <anisahrab043@gmail.com>
2026-02-03 23:07:42 +0400

    [feat] Test fit statement

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import ReactDom from "react-dom/client";
 import React from 'react';
 
 const x = 5;
-let text = 'Goodbye'
-if(x < 10){
-  text = 'If condition'
-}
-const Element = <h1>{text}</h1>;
+
+const Element = <h1>{(x) <10 ? "If statement!": "GoodBye"}</h1>;
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(Element);
