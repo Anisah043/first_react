bab090c92e6652a7d22bb67816b41dc21f0bb5f8
Hasina RABOANARY <anisahrab043@gmail.com>
2026-02-07 22:21:38 +0400

    [feat] Add form in react

---
+++ src/components/Form.js
@@ -0,0 +1,19 @@
+import {useState} from "react"
+function MyForm(){
+    const [name, setName] = useState("")
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        alert(`The name you entered was: ${name}`)
+    }
+    return(
+        <form onSubmit={handleSubmit}>
+            <label>Name:
+                <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)}/>
+            </label>
+            <input type="submit"/>
+        </form>
+    )
+}
+
+export default MyForm
--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import CarWithProps from "./components/CarWithProps";
 import Garage from "./components/Garage";
 import Click from "./components/Click";
 import Goal from "./components/Condition";
+import MyForm from "./components/Form";
+
 const myElement = <CarWithProps color="blue" brand="Ford" />;
 const root = ReactDom.createRoot(document.getElementById('root'));
 // root.render(<Car/>);
 // root.render(<Goal isGoal={false}/>);
-const cars = [
-    { id: 1, brand: 'Ford' },
-    { id: 2, brand: 'BMW' },
-    { id: 3, brand: 'Audi' },
-]
-root.render(<Garage cars={cars} />);
+// const cars = [
+//     { id: 1, brand: 'Ford' },
+//     { id: 2, brand: 'BMW' },
+//     { id: 3, brand: 'Audi' },
+// ]
+// root.render(<Garage cars={cars} />);
+root.render(<MyForm />);
