f2685d4a73794926f1b99329c180132d8064bd8f
Hasina RABOANARY <anisahrab043@gmail.com>
2026-02-07 22:44:50 +0400

    [feat] Multiple keys form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,23 @@
 import {useState} from "react"
 function MyForm(){
-    const [name, setName] = useState("")
+    const [inputs, setInputs] = useState({})
 
+    const handleChange = (event) => {
+        const name = event.target.name
+        const value = event.target.value
+        setInputs(values => ({...values, [name]: value}))
+    }
     const handleSubmit = (event) => {
         event.preventDefault()
-        alert(`The name you entered was: ${name}`)
+        console.log('-------------------', inputs)
     }
     return(
         <form onSubmit={handleSubmit}>
             <label>Name:
-                <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)}/>
+                <input type="text" name="username" value={inputs.username || ""} onChange={handleChange}/>
+            </label>
+            <label>Age:
+                <input type="text" name="age" value={inputs.age || ""} onChange={handleChange}/>
             </label>
             <input type="submit"/>
         </form>
